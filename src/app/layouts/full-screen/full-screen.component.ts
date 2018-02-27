150765eae45e7db4e1e5702638400da5712be9c8
Elger van Boxtel <elgervb@gmail.com>
2018-02-27 09:55:37 +0100

    add full screen layout

---
+++ src/app/layouts/full-screen/full-screen.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'evb-full-screen',
+  template: `
+    <ng-content></ng-content>
+  `,
+  styleUrls: ['./full-screen.component.scss']
+})
+export class FullScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FullScreenComponent } from './full-screen/full-screen.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [FullScreenComponent]
+})
+export class LayoutsModule { }
